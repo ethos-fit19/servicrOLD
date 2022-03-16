a64dadb8fee516db83bb9fe06bf4ba3aae6d3e67
Isanjalee <silvaaihn.19@uom.lk>
2022-03-16 11:41:39 +0530

    Added service provider dashboard

---
+++ lib/pages/service_provider/component/list_title_customer.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../themes.dart';
+
+class ListTileCustom extends StatelessWidget {
+  final Color bgColor;
+  final String pathIcon, title, subTitle;
+  const ListTileCustom({
+    Key? key,
+    required this.bgColor,
+    required this.pathIcon, required this.title,
+    required this.subTitle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: bgColor,
+        ),
+        child: SvgPicture.asset("assets/icons/"+pathIcon,
+          width: 21.88,
+          height: 10.94,
+          fit: BoxFit.scaleDown,
+        ),
+
+      ),
+      title: Text(title,
+        style: textBold2,
+      ),
+      subtitle: Text(subTitle,
+        style: textBold,
+      ),
+    );
+  }
+}
